56e3e9291dbe0b0396fb105fe66cd38cd0c35ce7
KIMKUNGJONG <37282516+KIMKUNGJONG@users.noreply.github.com>
2018-12-09 03:57:52 +0900

    Updates

---
+++ precache-manifest.70d2dd2ae3e999096cb43d594b56f595.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/movie_app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f9521ac5b62e87334b5b",
+    "url": "/movie_app/static/js/runtime~main.f9521ac5.js"
+  },
+  {
+    "revision": "556c8240c572702391cf",
+    "url": "/movie_app/static/js/main.556c8240.chunk.js"
+  },
+  {
+    "revision": "c0d9810730c589e98819",
+    "url": "/movie_app/static/js/1.c0d98107.chunk.js"
+  },
+  {
+    "revision": "556c8240c572702391cf",
+    "url": "/movie_app/static/css/main.c42e5b17.chunk.css"
+  },
+  {
+    "revision": "3b256dd7df95e952cbefce8d9035febb",
+    "url": "/movie_app/index.html"
+  }
+];
